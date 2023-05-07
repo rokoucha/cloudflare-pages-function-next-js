4ce4dc9be04179e6e41ee8a33dd6bb0964317518
Rokoucha <git@rokoucha.net>
2023-05-07 11:38:54 +0900

    db exist check

--- src/app/api/route.tsx
+++ src/app/api/route.tsx
@@ -1,8 +1,10 @@
-import { NextResponse } from 'next/server.js'
+import { NextResponse } from 'next/dist/server/web/spec-extension/response.js'
 
 export const runtime = 'edge'
 
 export async function GET() {
+  console.log(process.env.DB)
+
   const data = { message: 'Hello from the API!' }
 
   return NextResponse.json({ data })
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,20 @@
+export const runtime = 'edge'
+
 export default async function Page() {
+  console.log(process.env.DB)
+
+  async function handleSubmit() {
+    'use server'
+
+    console.log(process.env.DB)
+  }
+
   return (
     <div>
-      <h1>Hello, Next.js!</h1>
+      <h1>Hello, Next.js with Server Actions!</h1>
+      <form action={handleSubmit}>
+        <button type="submit">Call server action</button>
+      </form>
     </div>
   )
 }
